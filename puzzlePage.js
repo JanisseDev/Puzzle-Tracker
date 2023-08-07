2c391e5032deb493d2f8c87f6ca9c7f335448e99
JanisseDev <janisse.dev@gmail.com>
2023-08-07 16:12:14 +0200

    Add Graph (WIP)

--- puzzlePage.js
+++ puzzlePage.js
@@ -4,6 +4,9 @@ function loadPage() {
     jsonData = null;
     let puzzleId = new URLSearchParams(window.location.search).get('id');
     jsonData = JSON.parse(window.localStorage.getItem(puzzleId));
+    jsonData.sessions.sort(function(a, b){
+        return new Date(a.sessionDate) - new Date(b.sessionDate);
+    })
     reloadData();
 }
 
@@ -17,21 +20,19 @@ function reloadData() {
     while (sessionsGrid.lastElementChild) {
         sessionsGrid.removeChild(sessionsGrid.lastElementChild);
     }
-    if (jsonData.sessions != null) {
-        sessionCount = jsonData.sessions.length;
-        jsonData.sessions.forEach(sessionData => {
-            addedPiecesCount += sessionData.addedPieces;
-            let [hours, minutes] = sessionData.duration.split(":");
-            totalTimeInMinutes += Number(hours)*60 + Number(minutes);
+    sessionCount = jsonData.sessions.length;
+    jsonData.sessions.forEach(sessionData => {
+        addedPiecesCount += sessionData.addedPieces;
+        let [hours, minutes] = sessionData.duration.split(":");
+        totalTimeInMinutes += Number(hours) * 60 + Number(minutes);
 
-            let sessionCell = sessionCellTemplate.cloneNode(true);
-            sessionCell.id = sessionData.id;
-            sessionCell.querySelector(".sessionCellDuration").innerText = `Session duration: ${sessionData.duration}`;
-            sessionCell.querySelector(".sessionCellAddedPieces").innerText = `Pieces added: ${sessionData.addedPieces}`;
-            sessionCell.hidden = false;
-            sessionsGrid.appendChild(sessionCell);
-        });
-    }
+        let sessionCell = sessionCellTemplate.cloneNode(true);
+        sessionCell.id = sessionData.id;
+        sessionCell.querySelector(".sessionCellDuration").innerText = `Session duration: ${sessionData.duration}`;
+        sessionCell.querySelector(".sessionCellAddedPieces").innerText = `Pieces added: ${sessionData.addedPieces}`;
+        sessionCell.hidden = false;
+        sessionsGrid.appendChild(sessionCell);
+    });
 
     // General info
     document.getElementById('puzzleTitle').innerText = jsonData.name;
@@ -40,7 +41,7 @@ function reloadData() {
     document.getElementById('progressCircle').style.setProperty('--fillValue', progressValue);
     document.getElementById('totalTime').innerText = toHoursAndMinutes(totalTimeInMinutes);
     var startedDaysAgo = Math.floor((new Date() - new Date(jsonData.creationDate)) / 86400000);   //86400000 ms in a day
-    switch(startedDaysAgo) {
+    switch (startedDaysAgo) {
         case 0: document.getElementById('startedAgo').innerText = "Today"; break;
         case 1: document.getElementById('startedAgo').innerText = "Yesterday"; break;
         default: document.getElementById('startedAgo').innerText = `${startedDaysAgo} days ago`; break;
@@ -48,18 +49,61 @@ function reloadData() {
     document.getElementById('sessionCount').innerText = sessionCount;
     document.getElementById('addedPieces').innerText = addedPiecesCount;
     document.getElementById('piecesLeft').innerText = jsonData.piecesCount - addedPiecesCount;
-    document.getElementById('piecesPerHour').innerText = Math.floor((addedPiecesCount / (totalTimeInMinutes/60)));
+    document.getElementById('piecesPerHour').innerText = Math.floor((addedPiecesCount / (totalTimeInMinutes / 60)));
+
+    // Graph
+    var graphData = {
+        values: [],
+        maxValue: 0,
+        title: ""
+    }
+    jsonData.sessions.forEach(sessionData => {
+        graphData.values.push(sessionData.addedPieces);
+    });
+    graphData.maxValue = Math.max(...graphData.values);
+    graphData.title = "Added pieces";
+    setupGraph(graphData);
 
     // Debug spoiler
     document.getElementById('debugJson').innerHTML = `<pre><code>${JSON.stringify(jsonData, null, "\t")}</code></pre>`;
 }
 
+function setupGraph(graphData) {
+    let sessionGraph = document.getElementById('sessionGraph');
+    sessionGraph.setAttribute("title", graphData.title);
+    while (sessionGraph.lastElementChild) {
+        sessionGraph.removeChild(sessionGraph.lastElementChild);
+    }
+    var currentSession = 0;
+    graphData.values.forEach(value => {
+        let sessionGraphCell = document.createElement("div");
+        sessionGraphCell.setAttribute("class", "sessionGraphCell");
+        sessionGraphCell.setAttribute("label", value);
+        sessionGraphCell.style.setProperty('height', Math.floor((value / graphData.maxValue) * 100) + "%");
+        sessionGraphCell.animate(
+            [
+                // keyframes
+                { 'height': "0%" },
+                { 'height': Math.floor((value / graphData.maxValue) * 100) + "%" },
+            ],
+            {
+                // timing options
+                duration: 600,
+                easing: "ease-in-out",
+                iterations: 1,
+            },
+        );
+        sessionGraph.appendChild(sessionGraphCell);
+        currentSession += 1;
+    });
+}
+
 function toHoursAndMinutes(totalMinutes) {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
-  
+
     return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-  }
+}
 
 function newSessionClicked() {
     document.getElementById('sessionDate').value = new Date().toISOString().substring(0, 16);
@@ -91,8 +135,8 @@ function createSession() {
         let newSessionData = {
             id: self.crypto.randomUUID(),
             sessionDate: new Date(sessionDate),
-            duration: form.elements["sessionDuration"].value,
-            addedPieces: form.elements["addedPieces"].value,
+            duration: duration,
+            addedPieces: addedPieces,
         }
         jsonData.sessions.push(newSessionData);
         form.reset();
